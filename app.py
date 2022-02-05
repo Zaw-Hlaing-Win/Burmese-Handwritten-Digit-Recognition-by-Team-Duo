55151fc462b79c58e519cf2aa5cf1f5d53c4c21d
Zaw Zaw Htut <72982246+Zaw-Hlaing-Win@users.noreply.github.com>
2022-02-05 22:41:10 +0630

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from streamlit_drawable_canvas import st_canvas
+import os
+import numpy as np
+import cv2
+from tensorflow.keras.models import load_model
+
+model = load_model('bhdd.h5')
+
+st.title('Burmese Handwritten Digit Recognizer')
+st.markdown('''Write a digit!''')
+SIZE = 192
+
+canvas_result = st_canvas(
+    fill_color='#ffffff',
+    stroke_width=10,
+    stroke_color='#fffff',
+    background_color='#000000',
+    width=SIZE,
+    height=SIZE,
+    drawing_mode="freedraw" if mode else "transform",
+    key='canvas')
+
+if canvas_result.image_data is not None:
+    img = cv2.resize(canvas_result.image_data.astype('uint8'), (28, 28))
+    img_rescaling = cv2.resize(img, (SIZE, SIZE), interpolation=cv2.INTER_NEAREST)
+    st.write('Input Image')
+    st.image(img_rescaling)
+
+if st.button('Predict'):
+    x_test = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    pred = model.predict(test_x.reshape(1, 28, 28, 1))
+    st.write(f'result: {np.argmax(pred[0])}')
+    st.bar_chart(pref[0])
